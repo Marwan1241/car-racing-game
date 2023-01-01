248ef273547cfc5175d8282aeae9fb87eba3458b
Marwan Hawash <marwanmagdi.2001@gmail.com>
2023-01-01 15:39:09 +0200

    Refactored the code to function based

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-import * as THREE from "three";
+import * as THREE from "three/build/three.module.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader.js";
@@ -10,153 +10,179 @@ import front from "../res/front.jpg";
 import back from "../res/back.jpg";
 import roadmap from "../res/roadtest.jpg";
 
-//create instance of the renderer
-const renderer = new THREE.WebGLRenderer();
+// Function to set up the renderer
+function initRenderer() {
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  renderer.shadowMap.enabled = true;
+  return renderer;
+}
 
-//set renderer size to the size of the window
-renderer.setSize(window.innerWidth, window.innerHeight);
+// Function to set up the scene
+function initScene() {
+  scene = new THREE.Scene();
+  const textureLoader = new THREE.CubeTextureLoader();
+  const cubeTexture = textureLoader.load([
+    front,
+    back,
+    top,
+    bottom,
+    right,
+    left,
+  ]);
+  scene.background = cubeTexture;
+  return scene;
+}
 
-//add the renderer to the DOM
-document.body.appendChild(renderer.domElement);
+// Function to set up the camera
+function initCamera() {
+  camera = new THREE.PerspectiveCamera(
+    50,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.set(0, 10, 30); // set the camera position to (0, 20, 0)
+  return camera;
+}
 
-//set shadow map to true
-renderer.shadowMap.enabled = true;
+// Function to set up the orbit controls
+function initControls(camera) {
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.update();
+  return controls;
+}
 
-//create a new scene
-const scene = new THREE.Scene();
+// Function to set up the lights
+function initLights(scene) {
+  spotLight = new THREE.SpotLight(0xffffff, 2, 0, Math.PI / 6, 0.5, 1);
+  spotLight.position.set(-7, 20, 270);
+  spotLight.castShadow = true;
+  spotLight.shadow.mapSize.width = 1024;
+  spotLight.shadow.mapSize.height = 1024;
+  spotLight.shadow.camera.near = 0.5;
+  spotLight.shadow.camera.far = 500;
+  scene.add(spotLight);
+  return spotLight;
+}
 
-//load an image with texture loader
-const textureLoader = new THREE.CubeTextureLoader();
-const cubeTexture = textureLoader.load([front, back, top, bottom, right, left]);
+// Function to set up the plane
+function initPlane(scene) {
+  roadTexture = new THREE.TextureLoader().load(roadmap);
+  plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(15, 500),
+    new THREE.MeshStandardMaterial({
+      color: 0xffffff,
+      side: THREE.DoubleSide,
+      map: roadTexture,
+    })
+  );
+  plane.rotation.x = -0.5 * Math.PI;
+  roadTexture.wrapT = THREE.RepeatWrapping;
+  roadTexture.repeat.set(1, 100);
+  plane.receiveShadow = true;
+  scene.add(plane);
+  return plane;
+}
 
-//set scene background to the nebula image
-scene.background = cubeTexture;
-
-//create a new camera
-const camera = new THREE.PerspectiveCamera(
-  50,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-
-//create instance of the orbit
-const controls = new OrbitControls(camera, renderer.domElement);
-
-//axis helper serves as a guide to the scene
-const axesHelper = new THREE.AxesHelper(3);
-scene.add(axesHelper);
-
-camera.position.set(0, 3, -8);
-controls.update();
-
-//add a  spotlight to the scene that casts shadow
-const spotLight = new THREE.SpotLight(0xffffff, 2, 0, Math.PI / 6, 0.5, 1);
-spotLight.position.set(-7, 20, 270);
-spotLight.castShadow = true;
-spotLight.shadow.mapSize.width = 1024;
-spotLight.shadow.mapSize.height = 1024;
-spotLight.shadow.camera.near = 0.5;
-spotLight.shadow.camera.far = 500;
-scene.add(spotLight);
-
-//add plane with roadmap material and add it to the scene
-roadTexture = new THREE.TextureLoader().load(roadmap);
-
-const plane = new THREE.Mesh(
-  new THREE.PlaneGeometry(15, 500),
-  new THREE.MeshStandardMaterial({
-    color: 0xffffff,
-    side: THREE.DoubleSide,
-    map: roadTexture,
-  })
-);
-
-plane.rotation.x = -0.5 * Math.PI;
-
-roadTexture.wrapT = THREE.RepeatWrapping;
-roadTexture.repeat.set(1, 100);
-
-// plane.material.map  = roadTexture;
-plane.receiveShadow = true;
-
-scene.add(plane);
-
-//add a grid helper
-// const gridHelper = new THREE.GridHelper(20 , 20);
-// scene.add( gridHelper );
-
-//create instance of gltf loader
-const loader = new GLTFLoader();
-
-//create instace of rgbeloader
-const rgbeloader = new RGBELoader();
-
-//set the output of the output encoding
-renderer.outputEncoding = THREE.sRGBEncoding;
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
-renderer.toneMappingExposure = 0.4;
-
+// Function to set up the model
 let f50;
 
-//load the hdr file , comment this if you want to run faster
-rgbeloader.load(
-  "./car_models/MR_INT-005_WhiteNeons_NAD.hdr",
-  function (texture) {
-    texture.mapping = THREE.EquirectangularReflectionMapping;
-    scene.environment = texture;
+function initModel(scene) {
+  loader = new GLTFLoader();
 
-    //load a gltf resource
-    loader.load("./car_models/ff50.gltf", function (gltf) {
-      const model = gltf.scene;
-      scene.add(model);
-      f50 = model;
-      f50.traverse(function (node) {
-        if (node.isMesh) {
-          node.castShadow = true;
-          node.receiveShadow = true;
-        }
-      });
-    });
-  }
-);
+  loader.load("./car_models/ff50.gltf", function (gltf) {
+    f50 = gltf.scene;
+    f50.scale.set(2, 2, 2);
+    f50.castShadow = true;
+    f50.receiveShadow = true;
+    scene.add(f50);
+  });
+}
 
-//if you want the project to run faster uncomment the code below
-// loader.load("./car_models/ff50.gltf", function (gltf) {
-//   const model = gltf.scene;
-//   scene.add(model);
-//   f50 = model;
-//   f50.traverse(function (node) {
-//     if (node.isMesh) {
-//       node.castShadow = true;
-//       node.receiveShadow = true;
-//     }
-//   });
-// });
+function moveModelOnKeyPress(f50) {
+  // Set the speed at which the model moves
+  const speed = 0.01;
 
-//create eventlistner that moves the car and camera when keyboard is clicked
-document.addEventListener("keydown", function (e) {
-  if (e.key === "ArrowUp") {
-    f50.position.z += 1;
-    camera.position.z += 1;
-  }
-  if (e.key === "ArrowDown") {
-    f50.position.z -= 1;
-    camera.position.z -= 1;
-  }
-  if (e.key === "ArrowLeft") {
-    f50.position.x += 1;
-  }
-  if (e.key === "ArrowRight") {
-    f50.position.x -= 1;
-  }
-});
+  // Add event listener for keydown events
+  document.addEventListener("keydown", (event) => {
+    // Get the current position of the model
+    const currentPosition = f50.position;
+    const currentRotation = f50.rotation.y;
 
-//create function animate that rotate the cube
-function animate(time) {
-  //link scene and camera
+    // Check which key was pressed
+    if (event.code === "ArrowRight") {
+      // Move the model to the right
+      f50.position.x += speed;
+      // Rotate the model to the right
+      f50.rotation.y = currentRotation - 0.1;
+    } else if (event.code === "ArrowLeft") {
+      // Move the model to the left
+      f50.position.x -= speed;
+      // Rotate the model to the left
+      f50.rotation.y = currentRotation + 0.1;
+    } else if (event.code === "ArrowUp") {
+      // Move the model forward
+      f50.position.z += speed;
+    } else if (event.code === "ArrowDown") {
+      // Move the model backward
+      f50.position.z -= speed;
+    }
+  });
+}
+
+// Function to set up the hdr environment
+function initHDR(scene) {
+  const rgbeloader = new RGBELoader();
+  rgbeloader.load(
+    "./car_models/MR_INT-005_WhiteNeons_NAD.hdr",
+    function (texture) {
+      texture.mapping = THREE.EquirectangularReflectionMapping;
+      scene.environment = texture;
+    }
+  );
+}
+
+// Function to animate the model
+function animate() {
+  requestAnimationFrame(animate);
+  // the event listener for keypress events
+  moveModelOnKeyPress(f50);
+
   renderer.render(scene, camera);
 }
 
-//call the animate function
-renderer.setAnimationLoop(animate);
+function init() {
+  // Set up the renderer
+  const renderer = initRenderer();
+
+  // Set up the scene
+  const scene = initScene();
+
+  // Set up the camera
+  const camera = initCamera();
+
+  // Set up the orbit controls
+  initControls(camera);
+
+  // Set up the lights
+  initLights(scene);
+
+  // Set up the plane
+  initPlane(scene);
+
+  // Set up the model
+  initModel(scene);
+
+  // Set up the HDR environment
+  initHDR(scene);
+
+  // Render the scene
+  renderer.render(scene, camera);
+
+  // Animate the model
+  animate();
+}
+
+init();
